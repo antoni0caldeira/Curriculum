0c0e06a3d8d1ed9662bd0048e5cf5589e9d86e48
António Caldeira <antoniocaldeira@yahoo.com>
2024-08-15 09:59:19 +0100

    Add project files.

---
+++ CurriculumMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurriculumMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

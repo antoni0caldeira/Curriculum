1be3bf7a201413089056144509b063217d31d423
António Caldeira <antoniocaldeira@yahoo.com>
2024-08-23 19:22:15 +0100

    Models

---
+++ CurriculumMVC/Models/Certification.cs
@@ -0,0 +1,17 @@
+﻿namespace CurriculumMVC.Models
+{
+    public class Certification
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Issuer Issuer { get; set; }
+        public string Date { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CredentialID { get; set; }
+        public string CredentialURL { get; set; }
+        public string CredentialMedia { get; set; }
+        public string CredentialDescription { get; set; }
+
+        public Skill[] Skill { get; set; }
+    }
+}
---
+++ CurriculumMVC/Models/Language.cs
@@ -0,0 +1,11 @@
+﻿using CurriculumMVC.Models;
+using System;
+
+public class Language
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ProficiencyLevel Profiency { get; set; }
+
+}
+
